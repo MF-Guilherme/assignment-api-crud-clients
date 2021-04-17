b8a6f145f5f229b8b78f32e96a55fae98172b278
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2021-04-17 15:14:02 -0300

    DTO

--- backend/src/main/java/com/devsuperior/crudclients/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/crudclients/services/ClientService.java
@@ -1,22 +1,26 @@
 package com.devsuperior.crudclients.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.crudclients.dto.ClientDTO;
 import com.devsuperior.crudclients.entities.Client;
 import com.devsuperior.crudclients.repositories.ClientRepository;
 
 @Service
 public class ClientService {
-	
+
 	@Autowired
 	private ClientRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public List<Client> findAll() {
-		return repository.findAll();
+	public List<ClientDTO> findAll() {
+		List<Client> list = repository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+
 	}
 }
